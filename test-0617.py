b3e84a05c5f6195264c27a6c1cd100a87d804532
gyupp <kkb75281@gmail.com>
2026-06-18 08:50:07 +0900

    test3

--- test-0617.py
+++ test-0617.py
@@ -1,2 +1,4 @@
 print("Hello, World!")
 print("This is a test script for demonstration purposes.")
+print("It will print multiple lines of text to the console.")
+input("Press Enter to continue...")
