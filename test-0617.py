92782b8090c1a407567ff9ae2e809e54d5ce7ec8
gyupp <kkb75281@gmail.com>
2026-06-19 14:47:13 +0900

    test5

--- test-0617.py
+++ test-0617.py
@@ -4,3 +4,5 @@ print("It will print multiple lines of text to the console.")
 input("Press Enter to continue...")
 def greet_user(name):
     print(f"Hello, {name}! Welcome to the test script.")
+huser_name = input("Please enter your name: ")
+greet_user(huser_name)
